96a4b5938ae05cf0aa4a067da3275e96aac9d11c
Ian Shipman <ics@gambolingpangolin.com>
2019-10-15 19:00:04 -0500

    updates build script

--- polysemy/build.sh
+++ polysemy/build.sh
@@ -1,22 +1,45 @@
 #! /bin/sh
 
-pandoc \
-  --standalone \
-  -t Slidy \
-  -o dist/slides.html \
-  slides.md
+case $1 in
 
-haddock \
-  -o dist/game-0 \
-  --hyperlinked-source \
-  game-0/Main.hs
+  slides)
 
-haddock \
-  -o dist/game-1 \
-  --hyperlinked-source \
-  game-1/Main.hs
+    pandoc \
+      --self-contained \
+      --standalone \
+      -t Slidy \
+      -o dist/slides.html \
+      slides.md ;;
 
-haddock \
-  -o dist/game-2 \
-  --hyperlinked-source \
-  game-2/Main.hs
+  execs)
+
+    cabal v2-build \
+      --write-ghc-environment-files=always \
+      --enable-documentation ;;
+
+  sources)
+
+    mkdir -p dist/game-0
+    mkdir -p dist/game-1
+    mkdir -p dist/game-2
+
+    haddock \
+      -o dist/game-0 \
+      --hyperlinked-source \
+      game-0/Main.hs
+
+    haddock \
+      -o dist/game-1 \
+      --hyperlinked-source \
+      game-1/Main.hs
+
+    haddock \
+      -o dist/game-2 \
+      --hyperlinked-source \
+      game-2/Main.hs ;;
+
+  *)
+
+    echo "USAGE: ./build.sh (slides|execs|sources)" ;;
+
+esac
