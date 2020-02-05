2f0418ae457879b4b823b5817e97401a1f347857
Ian Shipman <ics@gambolingpangolin.com>
2020-02-05 08:13:33 -0600

    removes html slide generation

--- taproot/build.sh
+++ taproot/build.sh
@@ -3,13 +3,6 @@
 mkdir -p dist
 
 pandoc \
-  --self-contained \
-  --standalone \
-  -t Slidy \
-  -o dist/slides.html \
-  slides.md
-
-pandoc \
   --standalone \
   -t beamer \
   -o dist/slides.pdf \
